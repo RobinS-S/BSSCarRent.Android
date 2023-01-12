660008eb375e5048cdcb0c0de40ec64b772c02aa
VelocityMaximus <50678949+VelocityMaximus@users.noreply.github.com>
2023-01-12 13:33:27 +0100

    [ADD] light sensor and theme selector

--- app/src/main/java/com/bss/carrent/MainActivity.kt
+++ app/src/main/java/com/bss/carrent/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.bss.carrent
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.Menu
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
@@ -18,15 +24,19 @@ import com.bss.carrent.misc.AuthHelper
 import com.bss.carrent.misc.Helpers
 import com.bss.carrent.ui.auth.LoginViewModel
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import java.io.IOException
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var viewModel: LoginViewModel
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var authHelper: AuthHelper
+    private lateinit var switchAutoTheme: SwitchCompat
+    private lateinit var switchDarkTheme: SwitchCompat
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var lightSensor: Sensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         if (savedInstanceState != null) {
@@ -47,10 +57,6 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -58,11 +64,40 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_car_list, R.id.nav_rentals, R.id.nav_invoices, R.id.nav_register, R.id.nav_login
+                R.id.nav_car_list, R.id.nav_rentals, R.id.nav_invoices, R.id.nav_register, R.id.nav_login, R.id.switch_auto_theme, R.id.switch_dark_theme
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val menuItemAutoTheme = navView.menu.findItem(R.id.switch_auto_theme)
+        val menuItemDarkTheme = navView.menu.findItem(R.id.switch_dark_theme)
+
+        switchAutoTheme = menuItemAutoTheme.actionView as SwitchCompat
+        switchAutoTheme.isChecked = true
+
+        switchDarkTheme = menuItemDarkTheme.actionView as SwitchCompat
+        switchDarkTheme.isChecked = false
+
+
+        switchAutoTheme.setOnClickListener() {
+            switchDarkTheme.isEnabled = !switchAutoTheme.isChecked
+
+            if (switchDarkTheme.isEnabled) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchDarkTheme.isChecked = false
+            }
+        }
+
+        switchDarkTheme.setOnCheckedChangeListener { _, value ->
+            if (!switchAutoTheme.isChecked) {
+                switchDarkTheme.isClickable = true
+                when (value) {
+                    true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+        }
 
         authHelper = AuthHelper(applicationContext)
 
@@ -83,6 +118,9 @@ class MainActivity : AppCompatActivity() {
                 viewModel.setUser(null)
             }
         }
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -91,8 +129,39 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_LIGHT) {
+            if (this.switchAutoTheme.isChecked) {
+                val lux = event.values.get(0)
+                if (lux > 30000) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    switchDarkTheme.isChecked = false
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    switchDarkTheme.isChecked = true
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //nothing!
     }
 }
